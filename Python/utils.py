15784cd8f02db916a0c2756e066343f998ee0556
Naama <naamaberman@gmail.com>
2019-04-11 21:51:45 +0300

    Fixed server communication and color images!!!

--- Python/utils.py
+++ Python/utils.py
@@ -1,6 +1,8 @@
 import cv2
 import socket
 import sys
+from matplotlib.patches import Circle
+import matplotlib.pyplot as plt
 
 
 def take_image(output_path):
@@ -13,7 +15,7 @@ def take_image(output_path):
     # Close device
     video_capture.release()
     try:
-        cv2.imwrite(output_path + "frame.jpg", frame)  # save frame as JPEG file
+        cv2.imwrite(output_path + r"\frame.jpg", frame)  # save frame as JPEG file
     finally:
         pass
     return ret
@@ -27,3 +29,15 @@ def initialize_socket():
     sock.bind(server_address)
     return sock
 
+
+def draw_circles(image_path, points, circle_size, alpha, tag):
+    fig, ax = plt.subplots(1)
+    img = plt.imread(image_path)
+    ax.set_aspect('equal')
+    ax.imshow(img)
+    plt.axis('off')
+    for x, y in points:
+        circ = Circle((x, y),  circle_size, alpha=alpha, color=tag)
+        ax.add_patch(circ)
+    plt.savefig(image_path)
+    # plt.show()
