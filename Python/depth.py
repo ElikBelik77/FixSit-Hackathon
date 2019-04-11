afe6bfc9fd8e8e5e1a54ea9a56a69433599d9d4a
kanerha <49490637+kanerha@users.noreply.github.com>
2019-04-11 12:34:00 +0300

    #initail python commit

---
+++ Python/depth.py
@@ -0,0 +1,11 @@
+import cv2
+def depth (height,width):
+    f=330/height;
+    width*=f;
+    if (width>=190) and (width<=225):
+        return 0;
+    elif width<190:
+        return 1;
+    else:
+        return -1;
+    
