2be73072cffffc8b48470e490a48ba4163ab1075
Naama <naamaberman@gmail.com>
2019-04-11 11:01:20 +0300

    Added image capture.

--- Python/main.py
+++ Python/main.py
@@ -1,4 +1,6 @@
 # External lib
+import cv2
+
 
 # Internal lib
 import depth
@@ -6,8 +8,22 @@ import posture_model_connection
 
 
 def take_image():
-    pass
+    video_capture = cv2.VideoCapture(0)
+    # Check success
+    if not video_capture.isOpened():
+        raise Exception("Could not open video device")
+    # Read picture. ret === True on success
+    ret, frame = video_capture.read()
+    # Close device
+    video_capture.release()
+    cv2.imwrite("frame.jpg", frame)  # save frame as JPEG file
+
+
+
+
+
 
 
 if __name__ == '__main__':
-    pass
+    take_image()
+
