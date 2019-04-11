8cbcb178f35bc60107def5c968d8f5b9ef88d57d
Yonatan Lifshitz <yonlif@gmail.com>
2019-04-11 12:50:03 +0300

    Fixed coding style.

--- Python/depth.py
+++ Python/depth.py
@@ -1,11 +1,13 @@
 import cv2
-def depth (height,width):
-    f=330/height;
-    width*=f;
-    if (width>=190) and (width<=225):
-        return 0;
-    elif width<190:
-        return 1;
+
+
+def depth(height, width):
+    f = 330 / height
+    width *= f
+    if (width >= 190) and (width <= 225):
+        return 0
+    elif width < 190:
+        return 1
     else:
-        return -1;
-    
+        return -1
+
