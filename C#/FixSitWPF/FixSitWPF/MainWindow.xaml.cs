95edbff1c63061f4e4c24cdea83329757f0aec6a
jonathan diamant <jonathan.diamant1@gmail.com>
2019-04-11 13:05:00 +0300

    create home  page

--- C#/FixSitWPF/FixSitWPF/MainWindow.xaml.cs
+++ C#/FixSitWPF/FixSitWPF/MainWindow.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using MahApps.Metro.Controls;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -13,16 +14,30 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 
+
 namespace FixSitWPF
 {
     /// <summary>
     /// Interaction logic for MainWindow.xaml
     /// </summary>
-    public partial class MainWindow : Window
+    public partial class MainWindow : MetroWindow
     {
+        
         public MainWindow()
         {
             InitializeComponent();
+            AppMenu.AddButton(new FixSitWPF.Views.Buttons.FixSitButton(Display));
+            AppMenu.AddButton(new FixSitWPF.Views.Buttons.WebCamButton(Display));
+            AppMenu.AddButton(new FixSitWPF.Views.Buttons.ExerciseButton(Display));
+            AppMenu.AddButton(new FixSitWPF.Views.Buttons.StatsButton(Display));
+            AppMenu.AddButton(new FixSitWPF.Views.Buttons.SettingsButton(Display));
+            AppMenu.AddButton(new FixSitWPF.Views.Buttons.QuitButton());
+            
+            
+            
         }
+
+
+        
     }
 }
---
+++ C#/FixSitWPF/FixSitWPF/Views/Buttons/ExerciseButton.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Media;
+
+namespace FixSitWPF.Views.Buttons
+{
+    class ExerciseButton : System.Windows.Controls.Button, MenuButton
+    {
+        Grid _ContentGrid;
+        public ExerciseButton(Grid _ContentGrid)
+        {
+            this._ContentGrid = _ContentGrid;
+            this.Content = "exercise";
+            this.Click += ExerciseButton_Click;
+            this.Background = new SolidColorBrush(Colors.Red);
+        }
+
+        private void ExerciseButton_Click(object sender, RoutedEventArgs e)
+        {
+            ShowContent(_ContentGrid);
+        }
+
+        public void ShowContent(Grid grid)
+        {
+            ExerciseContent content = new ExerciseContent();
+            grid.Children.Add(content);
+            Grid.SetColumn(content, 0);
+            Grid.SetRow(content, 0);
+
+        }
+    }
+}
