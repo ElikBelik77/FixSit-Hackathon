b76f44c04eab97196ad64626730abd1ddee9adc0
Naama <naamaberman@gmail.com>
2019-04-11 11:24:16 +0300

    Added initialize socket

--- Python/main.py
+++ Python/main.py
@@ -22,17 +22,21 @@ def take_image():
 
 
 
-
-
-
-
-if __name__ == '__main__':
+def initialize_socket():
     # Create a TCP/IP socket
     sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     server_address = ('localhost', 10000)
     print(sys.stderr, 'starting up on %s port %s' % server_address)
     sock.bind(server_address)
-    # Listen for incoming connections
+    return sock
+
+
+
+
+POSTURE_REQUEST = "posture"
+
+if __name__ == '__main__':
+    sock = initialize_socket()
     sock.listen(1)
     while True:
         # Wait for a connection
@@ -40,17 +44,13 @@ if __name__ == '__main__':
         connection, client_address = sock.accept()
         try:
             print(sys.stderr, 'connection from', client_address)
-
-            # Receive the data in small chunks and retransmit it
+            # Receive the request
             while True:
-                data = connection.recv(16)
-                print(sys.stderr, 'received "%s"' % data)
-                if data:
-                    print(sys.stderr, 'sending data back to the client')
-                    connection.sendall(data)
+                request = connection.recv(16)
+                if request == POSTURE_REQUEST:
+                    pass
                 else:
-                    print(sys.stderr, 'no more data from', client_address)
-                    break
+                    connection.send("Err")
         finally:
             # Clean up the connection
             connection.close()
