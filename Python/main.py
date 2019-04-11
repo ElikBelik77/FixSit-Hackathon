919076f99cff953e9feb04aecc42ae67a537e997
Yonatan Lifshitz <yonlif@gmail.com>
2019-04-11 17:21:47 +0300

    Minor changes and added image error.

--- Python/main.py
+++ Python/main.py
@@ -10,6 +10,8 @@ import utils
 
 POSTURE_REQUEST = "request"
 POSTURE_STATUS = "posture_status"
+ERROR_ANSWER = '{"answer":"error"}'
+CAMERA_ERROR_ANSWER = '{"answer":"camera_error"}'
 IMAGE_PATH = r"\Users\Naama\Desktop\fixSitting"
 MODEL_PATH = r"\Users\Naama\Downloads\openpose-1.4.0-win64-cpu-binaries\openpose-1.4.0-win64-cpu-binaries"
 
@@ -18,7 +20,7 @@ if __name__ == '__main__':
     sock.listen(1)
     while True:
         # Wait for a connection
-        print(sys.stderr, 'waiting for a connection1 ' )
+        print(sys.stderr, 'waiting for a connection')
         connection, client_address = sock.accept()
         try:
             print(sys.stderr, 'connection from', client_address)
@@ -32,11 +34,14 @@ if __name__ == '__main__':
                 request = str(connection.recv(size).decode('utf-8'))
                 parsed = json.loads(request)
                 if parsed[POSTURE_REQUEST] == POSTURE_STATUS:
-                    utils.take_image(IMAGE_PATH)
+                    succeed = utils.take_image(IMAGE_PATH)
+                    if not succeed:
+                        print(sys.stderr, 'could not take image - camera error')
+                        connection.send(f'{len(CAMERA_ERROR_ANSWER):05d}{CAMERA_ERROR_ANSWER}'.encode('utf-8'))
                     result = posture_logic.main_logic(IMAGE_PATH, MODEL_PATH)
                     connection.send(f"{len(result):05d}{result}".encode('utf-8'))
                 else:
-                    connection.send(f"00003Err".encode('utf-8'))
+                    connection.send(f'{len(ERROR_ANSWER):05d}{ERROR_ANSWER}'.encode('utf-8'))
         except Exception as e:
             print(sys.stderr, e)
         finally:
